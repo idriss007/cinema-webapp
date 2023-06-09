7d58e6c6cc8ac2c9216d6b6f484d767c42341114
Idris Uslu <idrisuslu.007@hotmail.com>
2023-06-09 12:58:27 +0300

    Update.

--- backend/src/clients/db.js
+++ backend/src/clients/db.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-// require("dotenv").config();
 
 // mongoose
 //   .connect("mongodb://localhost:27017/CinemaWebAppDB")
@@ -7,6 +6,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGODB_ATLAS_ADMIN_USERNAME}:${process.env.MONGODB_ATLAS_ADMIN_PASSWORD}@cluster0.dql4xln.mongodb.net/CinemaWebAppDB`
+    `mongodb+srv://${process.env.MONGODB_ATLAS_ADMIN_USERNAME}:${process.env.MONGODB_ATLAS_ADMIN_PASSWORD}
+    @${process.env.MONGODB_ATLAS_CLUSTER}`
   )
   .then(() => console.log("Connected to mongo server."));
